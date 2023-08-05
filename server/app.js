8f90fa1406e1f5910fda1000eb8615a8fbfb57c4
Fahyvor <favourokafor30@gmail.com>
2023-08-06 00:18:02 +0100

    Recent changes

--- server/app.js
+++ server/app.js
@@ -30,19 +30,19 @@ app.use('/', (req, res) => {
 
 const port = process.env.PORT || 6000;
 
-// const start = async () => {
-//   try {
-//     await connectDB(process.env.MONGO_URI);
-//     app.listen(port, () =>
-//       console.log(`Server is listening on port ${port}...`)
-//     );
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// start();
-
-app.listen(port, () =>
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+// app.listen(port, () =>
+//       console.log(`Server is listening on port ${port}...`)
+//     );
