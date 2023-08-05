9b2ea7656768839ec1961e7260c6e26f8c8123b1
Fahyvor <favourokafor30@gmail.com>
2023-08-05 23:07:16 +0100

    Few changes

--- server/app.js
+++ server/app.js
@@ -24,20 +24,25 @@ const authRouter = require('./routes/auth');
 app.use('/api', authRouter);
 
 app.use('/', (req, res) => {
+  // res.json(req.body);
   res.send('server');
 });
 
 const port = process.env.PORT || 6000;
 
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
+// const start = async () => {
+//   try {
+//     await connectDB(process.env.MONGO_URI);
+//     app.listen(port, () =>
+//       console.log(`Server is listening on port ${port}...`)
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// start();
+
+app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
